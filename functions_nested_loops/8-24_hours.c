24d1d6ae0ceb3d8324fd20449c6501e706124619
yoryo904 <lealjorge904@gmail.com>
2024-08-18 00:00:13 -0500

    8(?)

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,23 +9,25 @@
 
 void jack_bauer(void)
 {
- 	int hr, h, min, m;
+	int hr, h, min, m;
 
 	for (h = 0; h <= 2; hr++)
 	{
 		for (hr = 0; hr <= 9; hr++)
-		{	
-			for (m = 0; m <= 5; m++)
+		{
+			if ((h <= 1 && hr <= 9) || (h <= 2 && hr <= 3))
 			{
-				for (min = 0; min <= 9; min++)
+				for (m = 0; m <= 5; m++)
 				{
+					for (min = 0; min <= 9; min++)
+					{
 					_putchar(h + '0');
 					_putchar(hr + '0');
 					_putchar(58);
 					_putchar(m + '0');
 					_putchar(min + '0');
 					_putchar('\n');
-					
+					}
 				}
 			}
 		}
