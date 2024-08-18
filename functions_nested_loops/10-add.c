5c96d5d362649803f4d028d86af2035089118d18
yoryo904 <lealjorge904@gmail.com>
2024-08-18 03:26:09 -0500

    'till 98

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - to do an addition
+ * between 2 numbers.
+ * @n: first number.
+ * @m: second number.
+ *
+ * Return: Always the result
+ */
+
+int add(int a, int b)
+{
+
+	return (a + b);
+}
