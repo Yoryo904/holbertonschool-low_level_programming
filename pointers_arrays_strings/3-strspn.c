ecc2b5fca20fe4d11124b439edfd58f0354a2af0
yoryo904 <lealjorge904@gmail.com>
2024-09-15 11:37:31 -0500

    3(?)

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,35 @@
 #include "main.h"
+/**
+ *
+ */
 
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
 /**
  *
  *
  */
 
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+	int str_len = 0;
 
+	for (i = 0; s[i]; i++)
+	{
+		for (i= 0; accept[j]; i++)
+			if (s[i] == accept[j])
+			{
+				str_len = _strlen(s);
+				break;
+			}
+	}
+	return (str_len);
+}
