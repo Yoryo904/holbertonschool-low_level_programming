7986bdbd8ed4a315b9b0f363a2ec0e4021d140f4
yoryo904 <lealjorge904@gmail.com>
2024-09-17 12:54:51 -0500

    fixes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,10 @@ char *cap_string(char *str)
 	for (i = 0; str[i]; i++)
 	{
 		for (j = 0; separators[j]; j++)
-		{	
+		{
+			if (i == 0 && (str[i] >= 97 && str[i] <= 122))
+				str[i] = str[i] - 32;
+
 			if (str[i] == separators[j] && str[i + 1])
 			{
 				if (str[i + 1] >= 97 && str[i + i] <= 122)
