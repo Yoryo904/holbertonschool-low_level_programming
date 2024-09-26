4a7c67e13e0c901de3416261455378549f1f80df
yoryo904 <lealjorge904@gmail.com>
2024-09-26 09:14:38 -0500

    finish(?)

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **arrayIntegers = 0;
+	int i = 0;
+	int j = 0;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arrayIntegers = (int **) malloc(sizeof(int *) * height);
+
+	if (arrayIntegers == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		arrayIntegers[i] = (int *) malloc (sizeof(int) * width);
+		if (arrayIntegers[i] == NULL)
+			return (NULL);
+		for (j = 0; j < width; j++)
+		{
+			arrayIntegers[i][j] = 0;
+		}
+	}
+	return (arrayIntegers);
+
+				
+}
---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
