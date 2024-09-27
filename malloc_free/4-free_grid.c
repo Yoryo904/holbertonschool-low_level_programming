2127b067d9c331598560be9538578246c01ec4fb
yoryo904 <lealjorge904@gmail.com>
2024-09-27 18:33:00 -0500

    fix

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -3,7 +3,11 @@
 #include <stdlib.h>
 
 /**
+ * free_grid - free the 2 dimensional array set before in "3-alloc_grid.c"
+ * @grid: the array to be free
+ * @height: the size of the array to be free
  *
+ * Return: the entire array already free
  */
 
 void free_grid(int **grid, int height)
