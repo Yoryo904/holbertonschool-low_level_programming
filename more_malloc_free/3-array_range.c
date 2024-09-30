28ca3d268fc900ee548c0682e66e5f65c6acea75
yoryo904 <lealjorge904@gmail.com>
2024-09-30 18:47:18 -0500

    fix

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -12,17 +12,18 @@ int *array_range(int min, int max)
 {
 	int *arrayRange = NULL;
 	int i = 0;
+	int min_cp = 0;
 
 	if (min > max)
 		return (NULL);
 
-	arrayRange = (int *) malloc(sizeof(int) * max);
+	arrayRange = (int *) malloc(sizeof(int) * (max - min) + 1);
 
 	if (arrayRange == NULL)
 		return (NULL);
 
-	for (i = min; i < max; i++)
-		arrayRange[i] = i + min;
+	for (i = 0; min_cp <= max; i++, min_cp++)
+		arrayRange[i] = min_cp;
 
 	return (arrayRange);
 
