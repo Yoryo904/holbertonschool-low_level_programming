0bcb7efef880434c76896eff87e77bb06214ac53
yoryo904 <lealjorge904@gmail.com>
2024-09-30 19:29:41 -0500

    fix

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -12,18 +12,17 @@ int *array_range(int min, int max)
 {
 	int *arrayRange = NULL;
 	int i = 0;
-	int min_cp = 0;
 
 	if (min > max)
 		return (NULL);
 
-	arrayRange = (int *) malloc(sizeof(int) * (max - min + 1));
+	arrayRange = (int *) malloc(sizeof(int) * ((max - min) + 1));
 
 	if (arrayRange == NULL)
 		return (NULL);
 
-	for (i = 0; min_cp <= max; i++, min_cp++)
-		arrayRange[i] = min_cp;
+	for (i = 0; min + i <= max; i++)
+		arrayRange[i] = min + i;
 
 	return (arrayRange);
 
--- more_malloc_free/3-main.c
+++ more_malloc_free/3-main.c
@@ -40,8 +40,8 @@ int main(void)
 {
     int *a;
 
-    a = array_range(0, 98);
-    simple_print_buffer(a, 99);
+    a = array_range(1024, 2048);
+    simple_print_buffer(a, 2048);
     free(a);
     return (0);
 }
