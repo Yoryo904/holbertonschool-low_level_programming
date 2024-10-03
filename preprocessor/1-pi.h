c7560a7d3f7600ff8722c2b61b19430450ea5ff9
yoryo904 <lealjorge904@gmail.com>
2024-10-03 13:41:17 -0500

    fixes

--- preprocessor/1-pi.h
+++ preprocessor/1-pi.h
@@ -1,1 +1,8 @@
+#ifndef _1_PI_H
+#define _1_PI_H
+
+/* Macros */
+
 #define PI 3.14159265359
+
+#endif /* _1_PI_H */
--- preprocessor/4-sum.h
+++ preprocessor/4-sum.h
@@ -1,1 +1,8 @@
+#ifndef _4_SUM_H_
+#define _4_SUM_H_
+
+/* Macros */
+
 #define SUM(x, y) ((x) + (y))
+
+#endif /* _4_SUM_H_ */
