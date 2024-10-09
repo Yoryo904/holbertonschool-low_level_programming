c0a4270165af62c8fa57da933e1f828d7a4613eb
yoryo904 <lealjorge904@gmail.com>
2024-10-09 18:46:09 -0500

    fixes

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,13 +4,13 @@
  * print_name - prints the desire name with a function that will
  * modify the name
  * @name: the name to be print
- * 
+ * @f: the function that will modify name
+ *
  * Return: the printed name with the desire modifications
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL)
 		return;
-	f(name);	
+	f(name);
 }
-
